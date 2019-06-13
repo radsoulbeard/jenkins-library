8a036c4281b1f59c467d01cbc1995ccce2395863
Thorsten Duda <thorsten.duda@sap.com>
2019-06-13 12:10:50 +0200

    path detection

--- vars/mtaBuild.groovy
+++ vars/mtaBuild.groovy
@@ -126,11 +126,12 @@ void call(Map parameters = [:]) {
 
             echo "[INFO] Executing mta build calling Hugo: '${mtaCall}'."
 
-            def echoPath = 'echo \\$PATH'
+
             echo "Ich bin der andere '${PATH}'"
             sh """#!/bin/bash
+            echo \$PATH
             export PATH=./node_modules/.bin:${PATH}
-            $echoPath
+            echo \$PATH
             $mtaCall
             """
 
